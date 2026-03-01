b26ed00cf91883ed55dda53373aae18bc5d201d9
kingsly001 <kingslyrobbin@gmail.com>
2026-03-02 01:24:14 +0530

    Fix: Switch to app.use middleware for catch-all routing

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,10 @@ app.use(express.static(frontendPath));
 
 // 6. Handle SPA Routing (Optional)
 // Ensures that if a user refreshes a sub-page, it serves index.html
-app.get('/:any*', (req, res) => {
+app.get('*', (req, res) => {
+    res.sendFile(path.join(frontendPath, 'index.html'));
+}, (error, req, res, next) => {
+    // This error handler prevents the PathError from crashing the app
     res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
